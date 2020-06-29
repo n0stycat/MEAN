4039cf00181c3441c43471822324e9a10afda77b
n0sty <n0stycat@gmail.com>
2020-06-30 01:33:02 +0300

    added morgan, cors, bodyparser

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
+
 const app = express()
 
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
