13837924702de343593810527dfccd32fe83b5ae
n0sty <n0stycat@gmail.com>
2020-06-30 02:12:09 +0300

    Made a mongoose connection, added config file, need to check if new config doesnt cause any trouble
    { useNewUrlParser: true, useUnifiedTopology: true } - use new config, delete if connection to DB goes wrong

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
+const keys = require('./config/keys')
 
 const app = express()
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected.'))
+    .catch(error => console.log(error))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
