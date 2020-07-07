bfa9022501ffd82026356800380eb9ff81272251
n0sty <n0stycat@gmail.com>
2020-07-07 20:52:30 +0300

    added functions for orders and order filtration

--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,8 @@ const express = require('express')
 const controller = require('../controllers/order')
 const router = express.Router()
 
-router.get('/', controller.getAll)
-router.post('/', controller.create)
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll)
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create)
 
 
 module.exports = router
