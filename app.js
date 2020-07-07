4af7dc6bd3fc013feb46b4f08328cc4369416817
n0sty <n0stycat@gmail.com>
2020-07-07 22:47:31 +0300

    installed client

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(require('morgan')('dev'))
 app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
 app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
