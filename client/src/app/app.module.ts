ce8ef5d44713d13fbfb63f9c82e57f19b28a90e6
n0sty <n0stycat@gmail.com>
2020-07-08 23:52:45 +0300

    Made subscriptions, token management in login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
@@ -8,7 +9,6 @@ import {AppRoutingModule} from "./app-routing.module";
 import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
 import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
 import {RegisterPageComponent} from './register-page/register-page.component';
-
 
 @NgModule({
   declarations: [
@@ -22,9 +22,9 @@ import {RegisterPageComponent} from './register-page/register-page.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  aSub: Subscription
 
-  constructor() {
+
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -18,9 +25,31 @@ export class LoginPageComponent implements OnInit {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params['registered']) {
+        // Теперь вы можете зайти в систему используя свои данные
+      } else if (params['accessDenied']) {
+        // Для начала авторизуйтесь в системе
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.aSub) {
+      this.aSub.unsubscribe()
+    }
   }
 
   onSubmit() {
+    this.form.disable()
 
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
   }
 }
