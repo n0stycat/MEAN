1950f917521d61b424e2661392ba3767995f714c
n0sty <n0stycat@gmail.com>
2020-07-12 09:41:30 +0300

    Started loading of categories-page component

--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -2,3 +2,13 @@ export interface User {
   email: string,
   password: string
 }
+
+// "?" since backend creates those fields
+// so frontend dont know about them at the time of creation
+
+export interface Category {
+  name: string
+  imageSrc?: string
+  user?: string
+  _id?: string
+}
