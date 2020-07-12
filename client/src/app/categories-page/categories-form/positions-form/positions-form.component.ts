53d3aa7596411cc26130cfdfa208ba73cd5e2698
n0sty <n0stycat@gmail.com>
2020-07-12 13:37:07 +0300

    made Position service, modal window, expanded material service

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,16 +1,50 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {PositionService} from "../../../shared/services/position.service";
+import {Position} from "../../../shared/interfaces";
+import {MaterialInstance, MaterialService} from "../../../shared/classes/material.service";
 
 @Component({
   selector: 'app-positions-form',
   templateUrl: './positions-form.component.html',
   styleUrls: ['./positions-form.component.css']
 })
-export class PositionsFormComponent implements OnInit {
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy{
+  @Input('categoryId') categoryId: string
+  @ViewChild('modal') modalRef: ElementRef
+  positions: Position[] = []
+  loading = false
+  modal: MaterialInstance
 
-  constructor() {
+  constructor(private positionService: PositionService) {
   }
 
   ngOnInit(): void {
+    this.loading = true
+    this.positionService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions
+      this.loading = false
+    })
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  onSelectPosition(position: Position) {
+    this.modal.open()
+    console.log(position) // temporary to avoid errors in typescript
+  }
+
+  onAddPosition() {
+    this.modal.open()
+  }
+
+  onCancel() {
+    this.modal.close()
   }
 
 }
