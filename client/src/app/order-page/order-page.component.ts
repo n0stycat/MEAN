fa21b2ced56d1e1324f58139d8b8f120bc964e0d
n0sty <n0stycat@gmail.com>
2020-07-12 23:09:47 +0300

    Created basic functions for order-page, basic modal window

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrderService} from "./order.service";
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit{
 
-  constructor() { }
+  @ViewChild('modal') modalRef: ElementRef
+  modal: MaterialInstance
+  isRoot: boolean
 
-  ngOnInit(): void {
+  constructor(private router: Router,
+              private order: OrderService) {
   }
 
+  ngOnInit(): void {
+    this.isRoot = this.router.url === '/order'
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order'
+      }
+    })
+    console.log(this.order) // TODO delete later, just a fix for typescript error (unused injection)
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  open() {
+    this.modal.open()
+  }
+
+  cancel() {
+    this.modal.close()
+  }
+  submit() {
+    this.modal.close()
+  }
 }
---
+++ client/src/app/order-page/order.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class OrderService {
+  add() {
+  }
+
+  remove() {
+  }
+
+  clear() {
+  }
+}
