01433561a58843acc6fd04b791253e81a26ff1b2
n0sty <n0stycat@gmail.com>
2020-07-14 07:07:57 +0300

    Added modal window to history-page

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,18 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild} from '@angular/core';
 import {Order} from "../../shared/interfaces";
+import {MaterialInstance, MaterialService} from "../../shared/classes/material.service";
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
   @Input() orders: Order[]
+  @ViewChild('modal') modalRef: ElementRef
 
-  constructor() {
+  selectedOrder: Order
+  modal: MaterialInstance
+
+  ngOnDestroy() {
+    this.modal.destroy()
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => {
+      total += item.quantity * item.cost
+      return total
+    }, 0)
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order
+    this.modal.open()
+  }
+
+  closeModal() {
+    this.modal.close()
   }
 
 }
