f8ddbc42a7b0957de52344bf037ed41afb1489f0
n0sty <n0stycat@gmail.com>
2020-07-17 02:21:10 +0300

    Added backend to analytics page

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -52,8 +52,25 @@ module.exports.overview = async function (req, res) {
     }
 }
 
-module.exports.analytics = function (req, res) {
+module.exports.analytics = async function (req, res) {
+    try {
+        const allOrders = await Order.find({user: req.user.id}).sort({date: 1})
+        const ordersMap = getOrdersMap(allOrders)
 
+        const average = +(calculatePrice(allOrders) / Object.keys(ordersMap).length).toFixed(2)
+
+        const chart = Object.keys(ordersMap).map(label => {
+            // label == 13.07.2020
+            const gain = calculatePrice(ordersMap[label])
+            const order = ordersMap[label].length
+            return {label, order, gain}
+        })
+
+        res.status(200).json({average, chart})
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
 function getOrdersMap(orders = []) {
