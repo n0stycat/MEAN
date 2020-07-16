da5a6efa15ff6eae70edbebdf302d2fcdd468e5a
n0sty <n0stycat@gmail.com>
2020-07-17 02:37:08 +0300

    Added data to frontend on analytics page

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {AnalyticsService} from "../shared/services/analytics.service";
+import {AnalyticsPage} from "../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
   styleUrls: ['./analytics-page.component.css']
 })
-export class AnalyticsPageComponent implements OnInit {
+export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('gain') gainRef: ElementRef
+  @ViewChild('order') orderRef: ElementRef
 
-  ngOnInit(): void {
+  aSub: Subscription
+  average: number
+  pending = true
+
+
+  constructor(private service: AnalyticsService) {
+  }
+
+  ngAfterViewInit() {
+    this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
+      this.average = data.average
+      this.pending = false
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
   }
 
 }
