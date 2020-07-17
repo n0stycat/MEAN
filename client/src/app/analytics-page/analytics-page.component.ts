84af5e3768059e5145786e86f1c61c9644d4a2e2
n0sty <n0stycat@gmail.com>
2020-07-17 03:05:32 +0300

    Installed chart.js, Created graphs for gain and order information

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {AnalyticsService} from "../shared/services/analytics.service";
 import {AnalyticsPage} from "../shared/interfaces";
+import {Chart} from "chart.js"
 import {Subscription} from "rxjs";
 
 @Component({
@@ -22,8 +23,42 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit() {
+    const gainConfig: any = {
+      label: 'Выручка',
+      color: 'rgb(255, 99, 132)'
+    }
+
+    const orderConfig: any = {
+      label: 'Заказы',
+      color: 'rgb(54, 162, 235)'
+    }
+
+
     this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
       this.average = data.average
+
+      gainConfig.labels = data.chart.map(item => item.label)
+      gainConfig.data = data.chart.map(item => item.gain)
+
+      orderConfig.labels = data.chart.map(item => item.label)
+      orderConfig.data = data.chart.map(item => item.order)
+
+      // *** TEST ****
+      // gainConfig.labels.push('16.07.2020')
+      // gainConfig.data.push(1500)
+
+      // orderConfig.labels.push('16.07.2020')
+      // orderConfig.data.push(2)
+      // *** TEST ****
+
+      const gainCtx = this.gainRef.nativeElement.getContext('2d')
+      const orderCtx = this.orderRef.nativeElement.getContext('2d')
+      gainCtx.canvas.height = '300px'
+      orderCtx.canvas.height = '300px'
+
+      new Chart(gainCtx, createChartConfig(gainConfig))
+      new Chart(orderCtx, createChartConfig(orderConfig))
+
       this.pending = false
     })
   }
@@ -33,5 +68,24 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
       this.aSub.unsubscribe()
     }
   }
+}
 
+function createChartConfig({labels, data, label, color}) {
+  return {
+    type: 'line',
+    options: {
+      responsive: true
+    },
+    data: {
+      labels,
+      datasets: [
+        {
+          label, data,
+          borderColor: color,
+          steppedLine: false,
+          fill: false
+        }
+      ]
+    }
+  }
 }
